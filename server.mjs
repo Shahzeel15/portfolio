e4fe08cd2bf69524d5d35c31ae94e373ab0350c1
zeelshah15 <szeel1694@gmail.com>
2026-08-08 18:58:19 +0530

    fix

--- server.mjs
+++ server.mjs
@@ -6,7 +6,7 @@ import { fileURLToPath } from 'node:url';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 4173;
 const DIST_DIR = path.join(__dirname, 'dist');
 
 const MIME_TYPES = {
